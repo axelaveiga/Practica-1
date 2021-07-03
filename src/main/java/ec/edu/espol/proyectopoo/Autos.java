4658f4beae5f3fbeb561fa0656bcd8c1cbc39e05
Axel Aveiga <axelaveiga00@hotmail.com>
2021-07-02 21:58:12 -0500

    Feaure: Se creo la clase auto

---
+++ src/main/java/ec/edu/espol/proyectopoo/Autos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.proyectopoo;
+
+/**
+ *
+ * @author Axel
+ */
+public class Autos extends Vehiculo{
+    protected String vidrios;
+    protected String transmision;
+
+    public Autos(String placa, String marca, String modelo, String motor, int año, double recorrido, String color, String combustible, double precio) {
+        super(placa, marca, modelo, motor, año, recorrido, color, combustible, precio);
+    }
+    
+    
+}
