7a740f4e4766e83dbbd52095dcf1e24cbbcd593d
Axel Aveiga <axelaveiga00@hotmail.com>
2021-07-02 20:20:46 -0500

    Feature: Se creo la clase vehiculo

---
+++ src/main/java/ec/edu/espol/proyectopoo/Vehiculo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.proyectopoo;
+
+/**
+ *
+ * @author Axel
+ */
+public class Vehiculo {
+    protected String placa;
+    protected String marca;
+    protected String modelo;
+    protected String motor;
+    protected int año;
+    protected double recorrido;
+    protected String color;
+    protected String combustible;
+    protected double precio;
+    
+}
