d44d995cc1c49f52c020bc705df1c05856299e0a
Josue Vera <86860303+woldier12@users.noreply.github.com>
2021-07-03 17:42:51 -0500

    se implemento el metodo registrar vehiculo

--- src/main/java/ec/edu/espol/model/Autos.java
+++ src/main/java/ec/edu/espol/model/Autos.java
@@ -1,6 +1,8 @@
 
 package ec.edu.espol.model;
 
+import java.util.Scanner;
+
 
 /**
  *
@@ -30,5 +32,31 @@ public class Autos extends Vehiculo{
 
     public void setTransmision(String transmision) {
         this.transmision = transmision;
+    }
+    public static Vehiculo DatosAuto(Scanner sc){
+        System.out.println("Ingrese la placa del vehiculo: ");
+        String placa =sc.next();
+        System.out.println("Ingrese la marca del vehiculo: ");
+        String marca= sc.next();
+        System.out.println("Ingrese el modelo del vehiculo: ");
+        String modelo= sc.next();
+        System.out.println("Ingrese el tipo de motor que tiene el vehiculo: ");
+        String motor=sc.next();
+        System.out.println("Ingrese el año del vehiculo: ");
+        int año= sc.nextInt();
+        System.out.println("Ingrese el kilometraje del vehiculo: ");
+        double recorrido=sc.nextDouble();
+        System.out.println("Ingrese el color del vehiculo: ");
+        String color=sc.next();
+        System.out.println("Ingrese el tipo de combustible que usa el vehiculo: ");
+        String combustible=sc.next();
+        System.out.println("Ingrese el precio de su vehiculo: ");
+        double precio= sc.nextDouble();
+        System.out.println("Ingrese la transmision del vehiculo: ");
+        String transmision=sc.next();
+        System.out.println("Ingrese que tipos de vidrio tine su vehiculo: ");
+        String vidrios= sc.next();
+        Vehiculo auto= new Autos(vidrios,placa,marca,modelo,motor,año,recorrido,color,combustible,precio,transmision);
+        return auto;     
     }
 }
--- src/main/java/ec/edu/espol/model/Vendedor.java
+++ src/main/java/ec/edu/espol/model/Vendedor.java
@@ -5,43 +5,49 @@
  */
 package ec.edu.espol.model;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 /**
  *
  * @author Josue Vera
  */
-public class Vendedor extends Persona {
-
-    public Vendedor(String nombres, String apellidos, String correo, String organizacion, String clave) {
-        super(nombres, apellidos, correo, organizacion, clave);
+public class Vendedor extends Persona{
+    public Vendedor(String nombres, String apellidos, String correo, String organizacion, String clave){
+        super(nombres,apellidos,correo,organizacion,clave);
     }
-    public static Vendedor RegistarVendedor(Scanner sc) {
+    public static Vendedor RegistarVendedor(Scanner sc){
         System.out.println("Ingrese sus nombres: ");
-        String nombre = sc.next();
+        String nombre=sc.next();
         System.out.println("Ingrese sus Apellidos: ");
-        String apellidos = sc.next();
+        String apellidos=sc.next();
         System.out.println("Ingrese la Organizacion donde trabaja: ");
-        String organizacion = sc.next();
+        String organizacion=sc.next();
         System.out.println("Ingrese su correo electronico: ");
-        String correo = sc.next();
-        while (Persona.validarEmail(correo) == false) {
+        String correo=sc.next();
+        while (Persona.validarEmail(correo)==false){
             System.out.println("Ingrese correo electronico valido: ");
-            String correo_n = sc.next();
-            correo = correo.replaceAll(correo, correo_n);
+            String correo_n=sc.next();
+            correo=correo.replaceAll(correo,correo_n);
         }
         System.out.println("Ingrese su clave de acceso: ");
-        String contrasena = sc.next();
-
-        Vendedor v = new Vendedor(nombre, apellidos, organizacion, correo, contrasena);
+        String contrasena=sc.next();
+       
+        Vendedor v=new Vendedor(nombre,apellidos,organizacion,correo,contrasena);
         return v;
     }
-    
+         public Vehiculo RegistrarVehiculo(Scanner sc){
+         System.out.println("Ingrese el tipo de vehiculo que quiere registrar: ");
+         String tipo= sc.next();
+         if (tipo.equalsIgnoreCase("auto"))
+           Autos.DatosAuto(sc);
+         if (tipo.equalsIgnoreCase("moto"))
+           Motos.DatosMoto(sc);
+         if (tipo.equalsIgnoreCase("camioneta"))
+           Camioneta.DatosCamioneta(sc);
+        return null;
+     }
     @Override
-    public String toString() {
-        return "Nombres: " + this.nombres + " Apellidos: " + this.apellidos + " Correo electronico: " + this.correo + " Organizacion: " + this.organizacion;
-    }   
+    public String toString(){
+        return "Nombres: "+this.nombres+" Apellidos: "+this.apellidos+" Correo electronico: "+this.correo+" Organizacion: "+this.organizacion ;
+    }
 }
