dc84d63741d6a11ee2b270527cd95bb30ea88e90
Josue Vera <86860303+woldier12@users.noreply.github.com>
2021-07-02 22:15:39 -0500

    se modifico el constructor para probar el push

--- src/main/java/ec/edu/espol/proyectopoo/Autos.java
+++ src/main/java/ec/edu/espol/proyectopoo/Autos.java
@@ -13,9 +13,10 @@ public class Autos extends Vehiculo{
     protected String vidrios;
     protected String transmision;
 
-    public Autos(String vidrios, String placa, String marca, String modelo, String motor, int año, double recorrido, String color, String combustible, double precio) {
+    public Autos(String vidrios, String placa, String marca, String modelo, String motor, int año, double recorrido, String color, String combustible, double precio, String transmision) {
         super(placa, marca, modelo, motor, año, recorrido, color, combustible, precio);
         this.vidrios = vidrios;
+        this.transmision=  transmision;
     }
     
     
