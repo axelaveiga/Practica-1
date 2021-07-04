e1bd31f649d8a32df8a9637b60d42101314f56f5
Josue Vera <86860303+woldier12@users.noreply.github.com>
2021-07-03 20:03:59 -0500

    actualizacion del metodo registrar vehiculo

--- src/main/java/ec/edu/espol/model/Vendedor.java
+++ src/main/java/ec/edu/espol/model/Vendedor.java
@@ -1,10 +1,12 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ec.edu.espol.model;
 
 import java.util.Scanner;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.PrintWriter;
+
 /**
  *
  * @author Josue Vera
@@ -15,11 +17,11 @@ public class Vendedor extends Persona{
     }
     public static Vendedor RegistarVendedor(Scanner sc){
         System.out.println("Ingrese sus nombres: ");
-        String nombre=sc.nextLine();
+        String nombre=sc.next();
         System.out.println("Ingrese sus Apellidos: ");
-        String apellidos=sc.nextLine();
+        String apellidos=sc.next();
         System.out.println("Ingrese la Organizacion donde trabaja: ");
-        String organizacion=sc.nextLine();
+        String organizacion=sc.next();
         System.out.println("Ingrese su correo electronico: ");
         String correo=sc.next();
         while (Persona.validarEmail(correo)==false){
@@ -29,24 +31,18 @@ public class Vendedor extends Persona{
         }
         System.out.println("Ingrese su clave de acceso: ");
         String contrasena=sc.next();
-        String contrasenaConvertida=Persona.convertirSHA256(contrasena);
-        Vendedor v=new Vendedor(nombre,apellidos,organizacion,correo,contrasenaConvertida);
+       
+        Vendedor v=new Vendedor(nombre,apellidos,organizacion,correo,contrasena);
         return v;
     }
-    // Funcion: Agregar String a un archivo
-    //@autor Axel Aveiga
-    public void saveFile(String nomfile) {
-        try(PrintWriter pw = new PrintWriter(new FileOutputStream(new File(nomfile), true)))
-            {
-                pw.println(this.nombres +"|"+ this.apellidos +"|"+ this.organizacion+"|"+ this.correo +"|"+ this.clave);
-            }
-        catch(Exception e) {System.out.println(e.getMessage());
-                    }
-        }
-    
          public Vehiculo RegistrarVehiculo(Scanner sc){
          System.out.println("Ingrese el tipo de vehiculo que quiere registrar: ");
          String tipo= sc.next();
+         while ((tipo.equalsIgnoreCase("auto")||tipo.equalsIgnoreCase("moto")||tipo.equalsIgnoreCase("camioneta"))== false){
+             System.out.println("Ingrese si su vehiculo es: auto-moto-camioneta");
+             String tipon= sc.next();
+             tipo=tipo.replaceAll(tipon,tipon);  
+         }
          if (tipo.equalsIgnoreCase("auto"))
            Autos.DatosAuto(sc);
          if (tipo.equalsIgnoreCase("moto"))
